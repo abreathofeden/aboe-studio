6a1ef3b8103398f3c7cbadad4289376b1d193cf5
abreathofeden <camillelesourd@live.fr>
2026-07-17 19:19:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 /* Forme du tapis */
-function formeTapis(type) {
-    const carre = document.getElementById("tapis_carre");
-    const dressage = document.getElementById("tapis_dressage");
 
-    carre.style.display = "none";
-    dressage.style.display = "none";
+function formeTapis(type){
 
-    if (type === "carre") {
-        carre.style.display = "block";
-    } else {
-        dressage.style.display = "block";
+    const carre=document.getElementById("tapis_carre");
+
+    const dressage=document.getElementById("tapis_dressage");
+
+    carre.style.display="none";
+    dressage.style.display="none";
+
+    if(type==="carre"){
+
+        carre.style.display="block";
+
     }
+
+    else{
+
+        dressage.style.display="block";
+
+    }
+
 }
 
-/* Couleur du tapis (calque) */
-function couleurTapis(couleur) {
-    document.querySelector('.couleur').style.backgroundColor = couleur;
+/* Couleur */
+
+function couleurTapis(couleur){
+
+    document.getElementById("couleur_tapis").style.backgroundColor=couleur;
+
 }
 
-/* Input color */
-function pickerCouleur() {
-    const couleur = document.getElementById("picker").value;
-    couleurTapis(couleur);
+/* Picker */
+
+function pickerCouleur(){
+
+    couleurTapis(document.getElementById("picker").value);
+
 }
