617491b1770fa6bad4f67b4ea241b6fd36b7a723
abreathofeden <camillelesourd@live.fr>
2026-07-17 17:29:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function formeTapis(type) {
+    document.getElementById("tapis_carre").style.display = "none";
+    document.getElementById("tapis_dressage").style.display = "none";
+
+    if (type === "carre") {
+        document.getElementById("tapis_carre").style.display = "block";
+    } else {
+        document.getElementById("tapis_dressage").style.display = "block";
+    }
+}
+
+function couleurTapis(couleur) {
+    document.getElementById("tapis_carre").style.filter =
+        `drop-shadow(0 0 0 ${couleur}) saturate(200%) brightness(120%)`;
+
+    document.getElementById("tapis_dressage").style.filter =
+        `drop-shadow(0 0 0 ${couleur}) saturate(200%) brightness(120%)`;
+}
+
+function pickerCouleur() {
+    const couleur = document.getElementById("picker").value;
+    couleurTapis(couleur);
+}
